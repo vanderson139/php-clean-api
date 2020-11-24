d7bad6eb1d3b4944fae0dc207d8e670b98bd4fc6
Vanderson Lopes <vandersonlopes@yahoo.com.br>
2020-11-24 08:42:15 -0300

    refactor repository injection

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,14 +3,26 @@
 namespace Api\Controller;
 
 use Api\Repository\AccountRepository;
+use Http\Request;
+use Http\Response;
 
 class AccountController extends BaseController
 {
+    /**
+     * @var AccountRepository
+     */
+    private $accountRepository;
+
+    public function __construct(Request $request, Response $response, AccountRepository $accountRepository)
+    {
+        parent::__construct($request, $response);
+        $this->accountRepository = $accountRepository;
+    }
+
     public function balance()
     {
         $id = $this->request->getParameter('account_id', '');
-        $repository = new AccountRepository();
-        $account = $repository->find($id);
+        $account = $this->accountRepository->find($id);
 
         if(empty($account)) {
             $this->response->setStatusCode(404, '0');

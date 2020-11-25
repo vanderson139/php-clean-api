0c05ddbde292a3364fbbee1e99f9d6b68c867f45
Vanderson Lopes <vandersonlopes@yahoo.com.br>
2020-11-24 21:21:23 -0300

    added account usecase mock test

--- tests/UseCase/AccountTest.php
+++ tests/UseCase/AccountTest.php
@@ -3,15 +3,32 @@
 namespace Tests\UseCase;
 
 use Core\UseCase\CreateAccountUseCase;
+use Core\UseCase\UpdateAccountUseCase;
 use PHPUnit\Framework\TestCase;
 
 class AccountTest extends TestCase
 {
     public function testCreateAccount()
     {
+        $data = ['balance' => 10];
+   
         $stub = $this->createMock(CreateAccountUseCase::class);
+   
         $stub->method('handle')
-            ->willReturn([]);
-        $this->assertEquals([], $stub->handle([]));
+            ->willReturn($data);
+   
+        $this->assertEquals($data, $stub->handle($data));
+    }
+
+    public function testUpdateAccount()
+    {
+        $data = ['balance' => 10];
+        
+        $stub = $this->createMock(UpdateAccountUseCase::class);
+   
+        $stub->method('addBalance')
+            ->willReturn($data);
+        
+        $this->assertEquals($data, $stub->addBalance(100, 10));
     }
 }
